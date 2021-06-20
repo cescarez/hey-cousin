af66211c708543d6bd22bb515ba33724e8b0b24c
Christabel Sebastian <christabel.escarez@gmail.com>
2021-06-19 21:35:24 -0700

    initialized project

---
+++ src/main/java/com/escarezapi/heycousin/HeyCousinApplication.java
@@ -0,0 +1,13 @@
+package com.escarezapi.heycousin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyCousinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyCousinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escarezapi/heycousin/HeyCousinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escarezapi.heycousin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyCousinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
