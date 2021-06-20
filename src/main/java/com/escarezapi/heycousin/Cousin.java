bc82bf1313dbd4b0dfcf59fa9143ab00929997ef
Christabel Sebastian <christabel.escarez@gmail.com>
2021-06-20 00:24:41 -0700

    added skeleton of CRUD api and of Person

---
+++ src/main/java/com/escarezapi/heycousin/Cousin.java
@@ -0,0 +1,37 @@
+package com.escarezapi.heycousin;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+public class Cousin {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Cousin parentOne;
+    private Cousin parentTwo;
+    List<String> nicknames;
+
+    // TODO: validate inputs
+    public Cousin(String firstName, String lastName) {
+        this.id = "0"; //TODO:create algo to create id
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate.toString();
+    }
+    public int getAge() {
+        return Period.between(LocalDate.now(), birthDate).getYears();
+    }
+}
--- src/main/java/com/escarezapi/heycousin/HeyCousinApplication.java
+++ src/main/java/com/escarezapi/heycousin/HeyCousinApplication.java
@@ -2,9 +2,9 @@ package com.escarezapi.heycousin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,8 +15,37 @@ public class HeyCousinApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 
+	@GetMapping("/cousins")
+	List<Cousin> getAllCousins() {
+	    List<> allCousins; //call database and return all names -- implement generics here
+		return allCousins;
+	}
+
+	@GetMapping("/cousin/{id}")
+	Cousin getCousin(@PathVariable String id) {
+		Cousin found; //call database and search for cousin by id, assigning to local
+		return found ? found : "Cousin not found."; //return results of fuzzy search
+	}
+
+	@PostMapping("/cousin/{id}")
+	Cousin updateCousin(@RequestBody Cousin newCousin, @PathVariable String id) {
+	    //create new cousin
+		Cousin newest; //return latest Cousin in database
+		return newest;
+	}
+
+	@PutMapping("/cousin/{id}")
+	Cousin updateCousin(@RequestBody Cousin updatedCousin, @PathVariable String id) {
+		Cousin updated; // call database and
+		return updated;
+	}
+
+	@DeleteMapping("/cousin/{id}")
+	void deleteCousin(@PathVariable String id) {
+		//delete by id from database
+	}
 }
